3446a1cde39ba57462518d8c73b6e53254019228
FeLipeDu4rte <felipe.duartedm@gmail.com>
2024-07-22 17:39:34 -0300

    23/07

--- listarcidade.php
+++ listarcidade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="listar.css">
 </head>
 <body>
     <?php
@@ -13,29 +14,36 @@
         // Executa a consulta
         $result = mysqli_query($con, $sql);
         // Retorna apenas uma linha
-        $row = mysqli_fetch_array($result);
+        //$row = mysqli_fetch_array($result);
     ?>
-    <h1>Consulta de Cidades</h1>
-    <table align="center" border="1" width="500">
-        <tr>
-            <th>Código</th>
-            <th>Nome</th>
-            <th>Estado</th>
-            <th>Alterar</th>
-            <th>Deletar</th>
-        </tr>
-        <?php
-            while($row = mysqli_fetch_array($result)){
-                echo "<tr>";
-                echo "<td>".$row['id']."</td>";
-                echo "<td>".$row['nome']."</td>";
-                echo "<td>".$row['estado']."</td>";
-                echo "<td><a href ='alteraCidade.php?id=".$row['id']."'>Alterar</a></td>";
-                echo "<td><a href ='deletaCidade.php?id=".$row['id']."'>Deletar</a></td>";
-                echo "</tr>";
-            }
-        ?>
-    </table>
-    <p><a href="index.html">Voltar</a></p>
+    <div class="container">
+        <h1>Consulta de Cidades</h1>
+
+        <table>
+            <tr>
+                <th>Código</th>
+                <th>Nome</th>
+                <th>Estado</th>
+                <th>Alterar</th>
+                <th>Deletar</th>
+            </tr>
+            <?php
+                // Coloque aqui o seu código PHP para consultar e exibir os dados da tabela
+                while($row = mysqli_fetch_array($result)) {
+                    echo "<tr>";
+                    echo "<td>".$row['id']."</td>";
+                    echo "<td>".$row['nome']."</td>";
+                    echo "<td>".$row['estado']."</td>";
+                    echo "<td><a href ='alteraCidade.php?id=".$row['id']."'>Alterar</a></td>";
+                    echo "<td><a href ='deletaCidade.php?id=".$row['id']."'>Deletar</a></td>";
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+
+        <div class="footer">
+            <p><a href="index.html">Voltar</a></p>
+        </div>
+    </div>
 </body>
 </html>
--- listarcliente.php
+++ listarcliente.php
@@ -5,22 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="listar.css">
 </head>
 <body>
     <?php
         include('includes/conexao.php');
-        $sql = "SELECT * FROM cliente";
+        $sql = "SELECT cli.id, cli.nome nomecliente, cli.email, cli.ativo,
+                cid.nome nomecidade, cid.estado
+                FROM cliente cli
+                lEFT JOIN cidade cid on cid.id = cidade_id";
         // Executa a consulta
         $result = mysqli_query($con, $sql);
         // Retorna apenas uma linha
-        $row = mysqli_fetch_array($result);
+        //$row = mysqli_fetch_array($result);
     ?>
+    <div class="container">
     <h1>Consulta de Clientes</h1>
     <table align="center" border="1" width="500">
         <tr>
             <th>Código</th>
             <th>Nome</th>
             <th>Email</th>
+            <th>Ativo</th>
             <th>Alterar</th>
             <th>Deletar</th>
         </tr>
@@ -28,14 +34,18 @@
             while($row = mysqli_fetch_array($result)){
                 echo "<tr>";
                 echo "<td>".$row['id']."</td>";
-                echo "<td>".$row['nome']."</td>";
+                echo "<td>".$row['nomecidade']."</td>";
                 echo "<td>".$row['email']."</td>";
+                echo "<td>".($row['ativo'] == 1 ? 'Sim': 'Não')."</td>";
                 echo "<td><a href ='alteraCliente.php?id=".$row['id']."'>Alterar</a></td>";
                 echo "<td><a href ='deletarCliente.php?id=".$row['id']."'>Deletar</a></td>";
                 echo "</tr>";
             }
         ?>
     </table>
-    <p><a href="index.html">Voltar</a></p>
+    <div class="footer">
+            <p><a href="index.html">Voltar</a></p>
+    </div>
+    </div>
 </body>
 </html>
